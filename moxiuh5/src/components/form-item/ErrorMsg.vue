0ec3507b06df215ae42d1d1ff36845bb91ec3152
Jing Chen <jingchen9412@gmail.com>
2019-12-08 04:08:28 +0800

    这几个字体大小是固定的

---
+++ moxiuh5/src/components/form-item/ErrorMsg.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="validation-error">
+    <span style="color: red">{{message}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ErrorMsg',
+    props: {
+      message: {
+        type: String,
+        required: true
+      }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .validation-error 
+    flex 1 0 atuo
+    padding 0 1rem
+</style>
